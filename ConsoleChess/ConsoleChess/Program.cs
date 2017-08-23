beaee94f259fc639db0e262e6b93c7023572340d
zzordel <zzordel@student.neumont.edu>
2017-08-23 13:44:26 -0600

    Change for Board Drawing

--- ConsoleChess/ConsoleChess/Program.cs
+++ ConsoleChess/ConsoleChess/Program.cs
@@ -10,30 +10,25 @@ namespace ConsoleChess
     {
         private static readonly string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H" };
         private const int size = 8;
-        private static bool[,] chessboard;
         static void Main(string[] args)
         {
             Chessboard();
 
         }
+
         private static void Chessboard()
         {
+
+
+
             const string top = " -----------------";
-
-            //init chessboard
-            chessboard = new bool[size, size];
-
-            for (int y = 0; y < size; y++)
+            const string line = "| | | | | | | | |";
+            for (int i = 0; i < size; i++)
             {
                 Console.WriteLine(" {0}", top);
-                Console.Write("{0} ", size - y);
-                for (int x = 0; x < size; x++)
-                {
-                    Console.Write("|{0}", chessboard[x, y] ? 'X' : ' ');
-                }
-                Console.WriteLine("|");
+                Console.WriteLine("{0} {1}", size - i, line);
             }
-
+            Console.WriteLine(" {0}", top);
             Console.Write("   ");
             for (int i = 0; i < size; i++)
             {
